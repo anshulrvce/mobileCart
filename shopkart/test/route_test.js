a074e8a723cc448d0cc06a934c93affe8f5fb91d
Namrata Gupta <ngup75@sapient.com>
2016-02-23 12:13:00 +0530

    added test case for cart

--- shopkart/test/route_test.js
+++ shopkart/test/route_test.js
@@ -13,12 +13,16 @@ mongoose.connect(connectionString);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var sampleData = {
+var sampleDataProduct = {
 	"id": "iphone5c",
 	"name": "iPhone 5c",
 	"capacity": "16 GB",
 	"price": 50000,
-	"image": "images/iphone_5c.png"
+	"image": "iphone_5c.png"
+}
+
+var sampleDataCart = {
+	"id": "iphone5c"
 }
 
 describe("Product route : ", function(){
@@ -39,7 +43,31 @@ describe("Product route : ", function(){
 	it('should respond with redirect on post', function(done){
 		request(app)
 					.post('/api/products')
-					.send(sampleData)
+					.send(sampleDataProduct)
+					.expect(200)
+					.expect('content-Type', 'application/json; charset=utf-8')
+					.end(function(err, res){
+						if(err)
+							throw err;
+						done();
+
+					});
+	});
+	it('cart route should return data', function(done){
+		request(app)
+					.get('/api/cart')
+					.expect('content-Type', 'application/json; charset=utf-8')
+					.expect(200)
+					.end(function(err, res){
+						if(err)
+							throw err;
+						done();
+					})
+	});
+	it('should respond with redirect on post', function(done){
+		request(app)
+					.post('/api/cart')
+					.send(sampleDataCart)
 					.expect(200)
 					.expect('content-Type', 'application/json; charset=utf-8')
 					.end(function(err, res){
